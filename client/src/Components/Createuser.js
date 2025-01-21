da70bf241e906e4c2950c949c4becb1967b7bc60
rgmanikandan07 <rgmanikandan07@gmail.com>
2025-01-21 11:36:17 +0530

    mern-todo

--- client/src/Components/Createuser.js
+++ client/src/Components/Createuser.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Createuser = () => {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
   const [age, setAge] = useState()
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/createUser", {name, email, age})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
 
   return (
     <>
      <div className='create-container'>
-      <form onSubmit={handelSubmit}>
+      <form onSubmit={handleSubmit}>
         <h2>Create user</h2>
         <label>Name :</label>
         <input type='text' placeholder='Enter Name' onChange={(e) => {setName(e.target.value)}}/>
